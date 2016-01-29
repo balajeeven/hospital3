d1c4518f820c9db925016544616c1be519110bc3
Balajee Venkatesh <balajee.venkatesh@bitwiseglobal.com>
2016-01-29 17:44:58 +0530

    Update HospitalAnalysis3.java

--- src/hospital/HospitalAnalysis3.java
+++ src/hospital/HospitalAnalysis3.java
@@ -1,27 +1,3 @@
 package hospial;
-import java.util.Properties;
-
-import cascading.flow.Flow;
-import cascading.flow.FlowDef;
-adoop.Hfs;
-import cascading.tuple.Fields;
-import cascading.tuple.Tuple;
-import cascading.tuple.TupleEntry;
 
 
-
-class Assembler extends SubAssembly{
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
-    public Assembler(){
-
-    }
-    public Assembler(Pipe inPipe,Fields fields){
-        setPrevious(inPipe);
-
-        }
-
-    }
-}
